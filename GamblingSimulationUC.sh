24bb736829d0a606055f53718ad2176088aa8d8b
Sangita <pal07sangita@gmail.com>
2020-07-31 10:58:37 +0530

    Added UC2:make bet to win or loose

--- GamblingSimulationUC.sh
+++ GamblingSimulationUC.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-echo "Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
-Stake=100
-bet=1
-=======
-#!/bin/bash
 echo "echo Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
 Stake=100
+#bet=1
 play()
 {
     echo $((RANDOM%2))
