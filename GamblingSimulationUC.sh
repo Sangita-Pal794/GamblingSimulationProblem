f9a9c569d1cee1ee771466f20d3d79cd1b81fd95
Sangita <pal07sangita@gmail.com>
2020-08-05 22:49:02 +0530

    Added UC5:function to calculate number of days won or lost

--- GamblingSimulationUC.sh
+++ GamblingSimulationUC.sh
@@ -1,78 +1,62 @@
 #!/bin/bash
-echo "echo Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
-Stake=100
-#counter for number of days
-no_of_days=0
-#counter for calculating the total amount won
-totalAmountWon=0
-#counter for calculating the total amount loss
-totalAmountLost=0
-#the amount gained or lost after every day gamble
-totalbalance=0
-maxBetAmount=$(($Stake+ ($Stake * 50/100)))
-minBetAmount=$(($Stake- ($Stake * 50/100)))
-play()
-{
-    echo $((RANDOM%2))
-}
-GambleResults()
-{
-no_of_days=0
-noofdayswon=0
-noofdayslost=0
 
-#creating a function to calculate the accounts for given number of days
-        while [ $no_of_days -lt 30 ] 
-	do
-	no_of_days=$(($no_of_days+1))
-#calling a function to calculate the result of gambling perDay
-		
-            currentBalance = returnsOfGamblingPerDay($maxBetAmount $minBetAmount)
+echo "echo Start Gambling With A Stake Of 100 every day and bet 1 in every game!!"
 
-            if [ $currentBalance == $maxBetAmount ]
-	    then
-                ((noofdayswon++))
-             else 
-                ((noofdayslost++))
-            #calling a function for printing after 20 days of gambling
-            printingAfter20Days($currentBalance $noOfdayswon $noofdayslost)
-       done
+declare -A dailyAmount
 
-        #printing the number of days won or lost and result after 30 day gamble
-        echo "Number of days won: " $noofdayswon
-        echo "Number of days lost: " $noofdayslost
+INITIAL_STAKE=100;
+BET=1;
+MAX_DAYS=28;
 
+totalPerDay=$((INITIAL_STAKE));
+finalAmt=0;
+newStake=0;
+stakeAmt=0;
+
+newStakePercent(){
+    stakePercentage=$(($newStake/2))
+    maxWin=$((newStake+stakePercentage));
+    maxLose=$((newStake-stakePercentage));
 }
 
-    #function to calculate the returns of gambling per day
-    returnsOfGamblingPerDay() {
-        #calculate the amount per gamble gamble
-        currentBalance = $Stake
-        while [ $maxBetAmount -gt $currentBalance && $currentBalance -gt $minBetAmount ] 
-	do
-            # calling the function within the if statement
-            bet=$(play)
-		if [[ $bet == 1 ]]
-                 then
-        	((currentBalance++))
-		else
-        	((currentBalance--))
-		fi
-     done
-     echo $currentBalance
-}
-    #function to print the per day gambling result after 20 days
-    printingAfter20Days(){
-        #displaying the won and lost amount after 20 days
-        if [ $no_of_days -ge 20 ]
-	then
-            echo "Total amount won after " $no_of_days " days:" totalCalculation($numofdayswon $currentBalance)
-            echo "Total amount lost after " $no_of_days  " days: " totalCalculation($numofdayslost $currentBalance)
+dailyCalculation(){
+    while [[ $totalPerDay -lt $maxWin && $totalPerDay -gt $maxLose ]]
+    do
+        result=$(($RANDOM%2))
+        if [[ $result -eq 1 ]]
+        then
+            ((totalPerDay++))
+        else
+            ((totalPerDay--))
         fi
-     }
-    #To calculate the total amount after investing
-    function totalCalculation()
-{
-        echo $(($no_of_days * $currentBalance))
+    done
 }
 
+totalAmount(){
+    for (( day=1; day<=$MAX_DAYS; day++ ))
+    do
+    newStake=$(($stakeAmt+$INITIAL_STAKE))
+    newStakePercent
+    dailyCalculation
+    wonOrLost $day
+    dailyAmount[$day]=$(($totalPerDay))
+    stakeAmt=$(($totalPerDay))
+    done
+}
+
+printDailyAmt(){
+    for ((day=1;day<=$MAX_DAYS;day++))
+    do
+        echo -e "Final Amount On Day" $day "\t" ${dailyAmount[$day]} "\n"
+    done
+}
+
+wonOrLost(){
+    if [[ $newStake -lt $totalPerDay ]]
+    then
+        echo "Start: $newStake You won on Day"$1 $(($totalPerDay-$newStake)) "End: $totalPerDay"
+    else
+        echo "Start: $newStake You lost on Day"$1 $(($newStake-$totalPerDay)) "End: $totalPerDay"
+    fi
+}
+totalAmount
