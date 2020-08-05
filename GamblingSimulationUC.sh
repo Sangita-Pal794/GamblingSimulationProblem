e9c524efc0bfcc7b060af254b6bf6d3789f2191b
Sangita <pal07sangita@gmail.com>
2020-08-05 23:23:57 +0530

    Added UC2:make bet to win or loose

--- GamblingSimulationUC.sh
+++ GamblingSimulationUC.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 echo "echo Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
+
 Stake=100
-play()
-{
-    echo $((RANDOM%2))
-}
-#Start Playing
-bet=$(play)
-if [[ $bet == 1 ]]
+BET=1
+
+result=$(($RANDOM%2))
+
+if [[ $result -eq 1 ]]
 then
-	((Stake++))
-	echo $Stake
+	echo "You won 1 dollar"
+	total=$(($stake+1))
 else
-	((Stake--))
-	echo $Stake
+	echo "You lost 1 dollar"
+	total=$(($stake-1))
 fi
