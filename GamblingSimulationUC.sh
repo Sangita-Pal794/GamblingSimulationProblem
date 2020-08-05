65f53046728366f50a46cdbab947a5d66ae39af7
Sangita <pal07sangita@gmail.com>
2020-08-05 23:31:44 +0530

    Added UC3:To check resign condition

--- GamblingSimulationUC.sh
+++ GamblingSimulationUC.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-echo "echo Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
-Stake=100
-#bet=1
-gameamount=0
-play()
-{
-    echo $((RANDOM%2))
-}
-#Start Playing
-while [ $Stake -ge 50 ] && [ $Stake -le 150 ]
-do
-bet=$(play)
-if [[ $bet == 1 ]]
-then
-	((Stake++))
-	echo $Stake
-else
-	((Stake--))
-	echo $Stake
-fi
-done
 
+echo "echo Start Gambling With A Stake Of $100 every day and bet $1 in every game!!"
+
+Stake=100;
+bet=1;
+maxWin=150;
+maxLose=50;
+
+total=$((Stake));
+
+while [[ $total -le $maxWin && $total -ge $maxLose ]]
+do
+	result=$(($RANDOM%2))
+	if [[ $result -eq 1 ]]
+	then
+		echo "You won 1 dollar"
+		((total++))
+	else
+		echo "You lost 1 dollar"
+		((total--))
+	fi
+done
